fa479366ff5077fcdc4329264901a6b58d6cebcf
stayboogy <stayboogy@email.com>
2022-06-02 17:15:06 -0500

    stayboogy: add lawnincons (not sure if it even works...)

--- aosp/12.1.0.r3/vendor/stayboogy/stayboogy.mk
+++ aosp/12.1.0.r3/vendor/stayboogy/stayboogy.mk
@@ -5,4 +5,9 @@ PRODUCT_PACKAGES += \
 
 include vendor/stayboogy/opensource/prebuilt/audio.mk
 
+# lawnicons (not sure if this even works honestly)
+include vendor/stayboogy/opensource/lawnicons/overlay.mk
 
+PRODUCT_PACKAGE_OVERLAYS += vendor/stayboogy/opensource/lawnicons/overlay
+
+# lawnicons end
